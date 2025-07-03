3394caf3f94b5a27ed3dbfb10a6423e101381e78
Akshada Sondekar <177552277+Akshada-Sondekar@users.noreply.github.com>
2025-07-03 20:23:24 +0530

    config.py

--- Backend(Flask)/config.py
+++ Backend(Flask)/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "---")  # Change this in production
+    SECRET_KEY = os.getenv("SECRET_KEY", "")  # Change this in production your secret key
     SESSION_TYPE = "filesystem"  # Session storage type
     DATABASE_NAME = "mentora.db"  # SQLite database
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "Your-gemini-api-key")  # Gemini API key
