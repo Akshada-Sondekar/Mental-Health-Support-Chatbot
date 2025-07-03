af8ac7852ebefe730336650d8abd8d4333fd9f1a
Akshada Sondekar <177552277+Akshada-Sondekar@users.noreply.github.com>
2025-07-03 20:24:23 +0530

    config.py

--- Backend(Flask)/config.py
+++ Backend(Flask)/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "")  # Change this in production your secret key
     SESSION_TYPE = "filesystem"  # Session storage type
     DATABASE_NAME = "mentora.db"  # SQLite database
-    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "Your-gemini-api-key")  # Gemini API key
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")  # Gemini API key
 
 class DevelopmentConfig(Config):
     DEBUG = True  # Enable debugging
